a5497fdb51f11587a788e294ec53d5fedc551e97
Nathan Castaldi <nathan@castaldi.dev>
2021-01-23 00:53:16 -0500

    created initial orm structure

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,15 @@
 const connection = requires("./connection.js");
+
+const orm = {
+    selectAll: function () {
+        const queryString = `SELECT * FROM burgers;`;
+
+        connection.query(queryString, function (err, data) {
+            if (err) throw err;
+
+            console.log(data);
+        })
+    }
+};
+
+module.exports = orm;
