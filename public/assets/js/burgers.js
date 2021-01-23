7e1400dda4847f35c5fd7a8b54779d089a32c7c8
Nathan Castaldi <nathan@castaldi.dev>
2021-01-23 13:07:49 -0500

    added function to handle form submit

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,24 @@
+$(function () {
+    $(".orderBurger").on("submit", function (event) {
+        // Make sure to preventDefault on a submit event.
+        event.preventDefault();
+
+
+        const newBurger = {
+            name: $("#newBurger").val().trim(),
+            devoured: false
+        };
+
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function () {
+                console.log("created new burger");
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+});
