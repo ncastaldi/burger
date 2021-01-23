d2c30229c6a4280bf3aea71d645c38aa3e919ce8
Nathan Castaldi <nathan@castaldi.dev>
2021-01-22 22:37:04 -0500

    updated seeds file

--- db/Seeds.sql
+++ db/Seeds.sql
@@ -1,1 +1,3 @@
--- Create include statements
+INSERT INTO burgers (burger_name) VALUES ("Single");
+INSERT INTO burgers (burger_name) VALUES ("Double");
+INSERT INTO burgers (burger_name) VALUES ("Triple");
