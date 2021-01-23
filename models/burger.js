ea850e7e78a1f8e08b276f95424304b77dc142f0
Nathan Castaldi <nathan@castaldi.dev>
2021-01-23 13:05:21 -0500

    added create method

--- models/burger.js
+++ models/burger.js
@@ -6,8 +6,14 @@ let burger = {
         orm.all("burgers", function (res) {
             cb(res);
         });
+    },
+    // The variables cols and vals are arrays.
+    create: function (cols, vals, cb) {
+        orm.create("burgers", cols, vals, function (res) {
+            cb(res);
+        });
     }
-}
+};
 
 // Export the database functions for the controller
 module.exports = burger;
