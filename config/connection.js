9eb19b86c80fc26736c3a2bd2b9812fec54800a0
Nathan Castaldi <nathan@castaldi.dev>
2021-01-23 13:31:30 -0500

    updated connection ahead of heroku deploy

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 // Require external npm packages
 const mysql = require("mysql");
+let connection;
 
 // Declare connection
-const connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Procmi*1", // ADD DATABASE PASSWORD
-    database: "burgers_db" // ADD DATABASE NAME
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "Procmi*1", // ADD DATABASE PASSWORD
+        database: "burgers_db" // ADD DATABASE NAME
+    });
+};
 
 // Make connection
 connection.connect(function (err) {
