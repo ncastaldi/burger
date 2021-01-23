8e8424bcc7b03a0e5f764f27e953970a9dc84fe1
Nathan Castaldi <nathan@castaldi.dev>
2021-01-23 03:15:45 -0500

    restructured syntax

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,24 @@
 // Require external npm packages
 const mysql = require("mysql");
 
-module.exports = {
-    makeConnection: function () {
-        // Declare connection
-        const connection = mysql.createConnection({
-            host: "localhost",
-            port: 3306,
-            user: "root",
-            password: "Procmi*1", // ADD DATABASE PASSWORD
-            database: "burgers_db" // ADD DATABASE NAME
-        });
+// Declare connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Procmi*1", // ADD DATABASE PASSWORD
+    database: "burgers_db" // ADD DATABASE NAME
+});
 
-        // Make connection
-        connection.connect(function (err) {
-            if (err) {
-                console.error("error connecting: " + err.stack);
-                return;
-            }
-
-            console.log("Database connected as id: " + connection.threadId);
-        });
+// Make connection
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
     }
-}
 
+    console.log("Database connected as id: " + connection.threadId);
+});
 
+// Export connection
+module.exports = connection;
