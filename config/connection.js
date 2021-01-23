325ed3cdb6e4ecd0338d1ec14e8075e1b6bbad0a
Nathan Castaldi <nathan@castaldi.dev>
2021-01-23 01:29:12 -0500

    added JAWSDB references

--- config/connection.js
+++ config/connection.js
@@ -2,17 +2,24 @@
 const mysql = require("mysql");
 
 module.exports = {
-    makeConnection: function () {
-        // Declare connection to database
-        const connection = mysql.createConnection({
-            host: "localhost",
-            port: 3306,
-            user: "root",
-            password: "Procmi*1", // ADD DATABASE PASSWORD
-            database: "burgers_db" // ADD DATABASE NAME
-        });
+    connect: function () {
+        // Declare connection
+        const connection;
 
-        // Make connection to db and log threadID to console
+        // Create connection
+        if (process.env.JAWSDB_URL) {
+            connection = mysql.createConnection(process.env.JAWSDB_URL);
+        } else {
+            connection = mysql.createConnection({
+                host: "localhost",
+                port: 3306,
+                user: "root",
+                password: "Procmi*1", // ADD DATABASE PASSWORD
+                database: "burgers_db" // ADD DATABASE NAME
+            });
+        }
+
+        // Make connection
         connection.connect(function (err) {
             if (err) {
                 console.error("error connecting: " + err.stack);
