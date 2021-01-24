6c37d2a2173fe954ca1038a8167f54f7d79244d3
Nathan Castaldi <nathan@castaldi.dev>
2021-01-23 20:10:36 -0500

    added update method

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
 
     // Declare DOM Variables
     const orderForm = $(".orderForm");
+    const eatButton = $(".change-devoured");
 
     // Declare JavaScript Variables
 
@@ -30,10 +31,34 @@ $(document).ready(function () {
                 location.reload();
             }
         );
+    };
+
+    function eatReturn() {
+        $(".change-devoured").on("click", function (event) {
+            const id = $(this).data("id");
+            const newState = $(this).data("newstate");
+
+            const newDevouredState = {
+                devoured: newState
+            };
+
+            // Send the PUT request.
+            $.ajax("/api/burgers/" + id, {
+                type: "PUT",
+                data: newDevouredState
+            }).then(
+                function () {
+                    console.log("changed devoured to", newState);
+                    // Reload the page to get the updated list
+                    location.reload();
+                }
+            );
+        });
     }
 
     // Function Calls
 
     // Define Event Listeners
     orderForm.on("submit", newBurger);
+    eatButton.on("click", eatReturn);
 })
