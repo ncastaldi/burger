22f29aba6082c38b37a36facdd4b198742af800d
Nathan Castaldi <nathan@castaldi.dev>
2021-01-25 17:03:07 -0500

    rewrote syntax to get rid of double-click bug

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,17 +1,8 @@
-$(document).ready(function () {
+$(function () {
+    $('.sidenav').sidenav();
 
-    // Declare DOM Variables
-    const orderForm = $(".orderForm");
-    const eatButton = $(".change-devoured");
+    $(".orderForm").on("submit", function (event) {
 
-    // Declare JavaScript Variables
-
-    // Function Definitions
-    function sidenav() {
-        $('.sidenav').sidenav();
-    };
-
-    function newBurger(event) {
         // Make sure to preventDefault on a submit event.
         event.preventDefault();
 
@@ -31,34 +22,27 @@ $(document).ready(function () {
                 location.reload();
             }
         );
-    };
+    });
 
-    function eatReturn() {
-        $(".change-devoured").on("click", function (event) {
-            const id = $(this).data("id");
-            const newState = $(this).data("devoured");
+    $(".change-devoured").on("click", function (event) {
+        const id = $(this).data("id");
+        const newState = $(this).data("devoured");
 
-            const newDevouredState = {
-                devoured: newState
-            };
+        const newDevouredState = {
+            devoured: newState
+        };
 
-            // Send the PUT request.
-            $.ajax("/api/burgers/" + id, {
-                type: "PUT",
-                data: newDevouredState
-            }).then(
-                function () {
-                    console.log("changed devoured to", newState);
-                    // Reload the page to get the updated list
-                    location.reload();
-                }
-            );
-        });
-    }
+        // Send the PUT request.
+        $.ajax("/api/burgers/" + id, {
+            type: "PUT",
+            data: newDevouredState
+        }).then(
+            function () {
+                console.log("changed devoured to", newState);
+                // Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
 
-    // Function Calls
-
-    // Define Event Listeners
-    orderForm.on("submit", newBurger);
-    eatButton.on("click", eatReturn);
-})
+});
